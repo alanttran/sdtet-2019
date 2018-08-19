2937636c4276f6fd2ba7ceb2972295d2498806fe
alanttran <mr.alan.tran@gmail.com>
2018-08-18 22:11:39 -0700

    separated out json from vendor, sponsor, pageant

--- src/components/sponsors/sponsors.vue
+++ src/components/sponsors/sponsors.vue
@@ -7,43 +7,28 @@
           <h1 id="sponsors">Sponsors</h1>
           <p>
             <b>The Sponsorship Application: </b>
-            <a target="_blank" href="https://s3-us-west-1.amazonaws.com/sdtet/documents/VAYA_Sponsorship_2018_v8.pdf">download</a>
+            <a target="_blank" v-bind:href="sponsorData.applicationURL">download</a>
           </p>
           <p>
             <b>Questions or details?: </b>
             <a href="mailto:sponsor@sdtet.com">sponsor@sdtet.com</a>
           </p>
-          <p>Dear Sponsors and Exhibitors,</p>
-          <p>For 11 years, VAYA has organized the Tết Festival, the largest Vietnamese
-            New Year event in San Diego and the second largest event of its kind
-            in Southern California. The Festival is one of the most anticipated annual
-            events for the Vietnamese community in San Diego. Highlights of the festival
-            include the Lion Dances, Miss Vietnam San Diego Pageant, Golden Voice
-            singing competition, the Cultural Village and various other performances.</p>
-          <p>This is a great opportunity for your business or organization to establish
-            an on-going presence in the Vietnamese community, introduce new products
-            or services and to tie non-profit investment strategy to your business
-            development plan. On average, our festival attracts more than 25,000
-            people each year.</p>
-          <p>Below are benefits of supporting the San Diego Tết Festival:
-            <ul>
-              <li>Increase your company’s visibility </li>
-              <li>Promote your company’s products and/or services </li>
-              <li>Explore new business ventures </li>
-              <li>Reinforce your commitment and support of the Vietnamese community</li>
-              <li>Celebrate the Vietnamese New Year</li>
+          <div v-for="text in sponsorData.text" :key="text.id">
+            <p>{{text}}</p>
+          </div>
+          <p>
+            <ul v-for="text in sponsorData.benefitsList" :key="text.id">
+              <li>{{text}}</li>
             </ul>
           </p>
-          <p>On behalf of VAYA, we would like to invite your company to take part in
-            celebrating the diversity of San Diego by pledging your support. Please
-            feel free to contact us should you have any further inquiries about our
-            festival.</p>
-          <p>VAYA</p>
+          <div v-for="text in sponsorData.closingText" :key="text.id">
+            <p>{{text}}</p>
+          </div>
         </v-flex>
 
         <v-flex xs12 md7 lg7 class="sdtet-text-align-center">
           <h1 class="sdtet-h1-spacer">&nbsp;</h1>
-          <h2>Thank you 2018 Sponsors!</h2>
+          <h2>{{sponsorData.slogan}}</h2>
           <v-layout class="sdtet-sponsors-logos-container" row wrap justify-space-around>
             <a v-for="sponsor in sponsors"  :key="sponsor.id" :href="sponsor.url" target="_blank">
               <img class="ma-3" :src="sponsor.logo_image_link">
@@ -58,11 +43,13 @@
 
 <script type="text/javascript">
   import axios from 'axios'
+  import data from './sponsors.json'
 
   export default {
     data () {
       return {
-        sponsors: []
+        sponsors: [],
+        sponsorData: data
       }
     },
     created () {
@@ -70,7 +57,7 @@
         .then(response => {
           // JSON responses are automatically parsed.
           this.sponsors = response.data
-          console.log('Sponsors: ', response.data)
+          //console.log('Sponsors: ', response.data)
         })
         .catch(e => {
           console.log(e)
--- src/components/vendors/vendors.vue
+++ src/components/vendors/vendors.vue
@@ -5,14 +5,10 @@
 			<v-layout row wrap>
 				<v-flex xs12 md6 lg6>
 					<h1>Vendors</h1>
-					<p>Dear Prospective Vendors,</p>
-					<p>Once again the Lunar New Year Festival has come! This year, we hope to bring you another fun, successful and profitable experience during our 3-day festival. Our previous vendors have made huge profits, and we would love to have you along for the ride.</p>
-					<p>With over 10 years in the running, we have become very well-known to the San Diego community as well as our neighboring cities. Our annual festival attracts well over 20,000 people of different cultures and ethnicities, making it one of the largest Lunar New Year Festivals in Southern California.</p>
-					<p>Don't hesitate to contact us if you have any questions. We'll make sure that the upcoming Lunar New Year will be an amazing experience for you.</p>
-					<p>VAYA</p>
-					<p>Questions or details?:
-							<a href="mailto:vendor@sdtet.com">vendor@sdtet.com</a>
-					</p>
+					<div v-for="text in vendorData.text" :key="text.id">
+						<p>{{text}}</p>
+					</div>
+					<p>Questions or details?: <a href="mailto:vendor@sdtet.com">{{vendorData.email}}</a></p>
 				</v-flex>
 				<v-flex xs12 md6 lg6 class="sdtet-text-align-center">
 					<div class="sdtet-h1-spacer"><h1>&nbsp;</h1></div>
@@ -34,11 +30,13 @@ import GameCommercialBooth from './types/game-commercial-booth'
 import InfoArtBooth from './types/info-art-booth'
 import FoodBeverageBooth from './types/food-beverage'
 import NonProfitGovBooth from './types/non-profit-gov'
+import data from './vendors.json'
 
 export default {
   data () {
     return {
-      dialog: false
+      dialog: false,
+      vendorData: data
     }
   },
   components: {
